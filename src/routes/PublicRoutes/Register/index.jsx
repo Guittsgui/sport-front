eea5875d58dd607acd17e645594c33cbba458a54
Guilherme Tavares <guilhermet.silva@viannasempre.com.br>
2023-05-25 22:01:22 -0300

    linkando os valores dos inputs aos states na page Register

--- src/routes/PublicRoutes/Register/index.jsx
+++ src/routes/PublicRoutes/Register/index.jsx
@@ -3,20 +3,56 @@ import FormContainer from './../../../components/FormContainer'
 import {Input} from '../../../components/input'
 import { Button } from '../../../components/button'
 import {Link} from 'react-router-dom'
+import { useState } from 'react'
 
 const Register = () => {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+
 
     function handleSubmitForm(event){
         event.preventDefault()
         alert('cadastrou')
     }
 
+    function handleChangeName(event){
+        setName(event.target.value)
+    }
+    function handleChangeEmail(event){
+        setEmail(event.target.value)
+    }
+    function handleChangePassword(event){
+        setPassword(event.target.value)
+    }
+    function handleChangeConfirmPassword(event){
+        setConfirmPassword(event.target.value)
+    }
+
     return <S.Container>
         <FormContainer title="Register">
-            <Input type='text' placeholder='Insira o seu Nome Completo'/>
-            <Input type='email' placeholder='Insira o seu Email'/>
-            <Input type='password' placeholder='Insira a sua Senha'/>
-            <Input type='password' placeholder='Repita a sua Senha'/>
+            <Input type='text' 
+            placeholder='Insira o seu Nome Completo'
+            value={name}
+            onChange={handleChangeName}/>
+
+            <Input type='email' 
+            placeholder='Insira o seu Email'
+            value={email}
+            onChange={handleChangeEmail}/>
+
+            <Input type='password' 
+            placeholder='Insira a sua Senha'
+            value={password}
+            onChange={handleChangePassword}/>
+
+            <Input type='password' 
+            placeholder='Repita a sua Senha'
+            value={confirmPassword}
+            onChange={handleChangeConfirmPassword}/>
+
             <Button onClick={handleSubmitForm}> Cadastrar </Button>
             <Link to="/login"> Já é cadastrado? Clique Aqui para Logar</Link>
 
