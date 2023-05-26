3f081f5aa011796179ca3e273cabd0e46b33c472
Guilherme Tavares <guilhermet.silva@viannasempre.com.br>
2023-05-25 21:54:46 -0300

    linkando inputs do login com seus States

--- src/routes/PublicRoutes/Login/index.jsx
+++ src/routes/PublicRoutes/Login/index.jsx
@@ -3,20 +3,42 @@ import FormContainer from '../../../components/FormContainer'
 import {Input} from '../../../components/input'
 import { Button } from '../../../components/button'
 import {Link} from 'react-router-dom'
+import { useState } from 'react'
 
 const Login = () => {
 
+
+    const [email, setEmail] = useState('')
+    const [password,setPassword] = useState('')
+
     function handleSubmitForm(event){
         event.preventDefault()
-        alert('enviou')
+        
+    }
+
+    function handleEmailChange(event){
+        setEmail(event.target.value)
+    }
+
+    function handlePasswordChange(event){
+        setPassword(event.target.value)
     }
 
     return <S.Container>
         
         <FormContainer title="Login">
-            <Input type="email" placeholder='Informe seu E-mail'/>
-            <Input type="password" placeholder='Informe sua Senha'/>
+            <Input type="email" 
+            placeholder='Informe seu E-mail'
+            value={email}
+            onChange={handleEmailChange}/>
+
+            <Input type="password" 
+            placeholder='Informe sua Senha'
+            value={password}
+            onChange={handlePasswordChange}/>
+
             <Button onClick={handleSubmitForm}> Login </Button>
+
             <Link> Esqeuceu sua Senha ? Clique Aqui </Link>
         </FormContainer>
         
